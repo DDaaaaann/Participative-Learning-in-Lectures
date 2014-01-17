2cf0dd363ff13ef8563173c737fe1050766a6fa8
PvdPal <patrickvanderpal@hotmail.com>
2014-01-17 15:38:38 +0100

    Antwoorden en voten ingevoerd

--- Django/courses/urls.py
+++ Django/courses/urls.py
@@ -4,18 +4,23 @@ from courses import views
 
 urlpatterns = patterns('',
     # url(r'^$', views.index, name='index'),
-    url(r'^(?P<course_id>\d+)/results/$', views.course_results, name='course_results'),
-    url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
-    url(r'^$', views.course_index, name='course_index'),
-    url(r'^(?P<course_id>\d+)/$', views.lecture_index,
-        name='lecture_index'),
-
-    url(r'^(?P<course_id>\d+)/(?P<lecture_id>\d+)/$', views.question_index, 
-        name='question_index'),
-
+# Waar is 'course_results' voor nodig? [Patrick]
+    url(r'^(?P<course_id>\d+)/results/$',
+        views.course_results, name='course_results'),
+    url(r'^$',
+        views.course_index, name='course_index'),
+    url(r'^(?P<course_id>\d+)/$',
+        views.lecture_index, name='lecture_index'),
+    url(r'^(?P<course_id>\d+)/(?P<lecture_id>\d+)/$',
+        views.question_index, name='question_index'),
+# Deze zou niet nodig moeten zijn, zolang de andere gebruikt wordt. [Patrick]
     url(r'^(?P<course_id>\d+)/(?P<lecture_id>\d+)/(?P<question_id>\d+)/$',
-        views.answer_index, 
-        name='answer_index'),
-   
+        views.answer_index, name='answer_index'),
+    url(r'^(?P<course_id>\d+)/(?P<lecture_id>\d+)/(?P<question_id>\d+)/vote/$',
+        views.vote, name='vote'),
+    url(r'^(?P<course_id>\d+)/(?P<lecture_id>\d+)/(?P<question_id>\d+)/results/$',
+        views.results, name='results'),
+# Deze zou niet nodig moeten zijn, zolang de andere gebruikt wordt. [Patrick]
+    url(r'^(?P<course_id>\d+)/(?P<lecture_id>\d+)/(?P<question_id>\d+)/answer/$',
+        views.answer, name='answer'),
 )
-
--- Django/courses/views.py
+++ Django/courses/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.views import generic
 
 from models import Course
 from models import Lecture
@@ -12,14 +15,12 @@ def course_results(request, course_id):
     response = "You're looking at the results of course %s."
     return HttpResponse(response % course_id)
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
 def course_detail(request, course_id):
     return HttpResponse("You're looking at course %s." % course_id)
 
 def course_index(request):
-    course_list = Course.objects.filter(teachers_id=request.user.id).order_by('-course_text')
+    course_list = Course.objects.order_by('course_text')
+    #course_list = Course.objects.filter(teachers_id=request.user.id).order_by('course_text')
 
     template = loader.get_template('courses/course_index.html')
 
@@ -33,13 +34,14 @@ def course_index(request):
 def lecture_index(request, course_id):
     course = Course.objects.get(id=course_id)
     #course = course.lectures.get(teacher_id=request.user.id)
-    lecture_list = course.lectures.filter(teacher_id=request.user.id).order_by('-lecture_text')
+    lecture_list = course.lectures.order_by('lecture_text')
+    #lecture_list = course.lectures.filter(teacher_id=request.user.id).order_by('-lecture_text')
 
     template = loader.get_template('courses/lecture_index.html')
 
     context = RequestContext(request, {
         'lecture_list': lecture_list,
-        'course_id' : course_id
+        'course_id' : course_id,
     })
 
     return HttpResponse(template.render(context))
@@ -48,12 +50,9 @@ def lecture_index(request, course_id):
 def question_index(request, course_id, lecture_id):
     lecture = Lecture.objects.get(id=lecture_id)
     question_list = lecture.questions.order_by('-pub_date')
-
     template = loader.get_template('courses/question_index.html')
-
-    print question_list
-
     context = RequestContext(request, {
+        'lecture': lecture,
         'question_list': question_list,
         'course_id': course_id,
         'lecture_id': lecture_id,
@@ -62,12 +61,10 @@ def question_index(request, course_id, lecture_id):
     return HttpResponse(template.render(context))
 
 
-def answer_index(request, question_id, course_id, lecture_id):
+def answer_index(request, course_id, lecture_id, question_id):
     question = Question.objects.get(id=question_id)
-    answer_list = question.answers.order_by('-answer_text')
-
-    template = loader.get_template('courses/answer_index.html')
-
+    answer_list = question.answers.order_by('answer_text')
+    template = loader.get_template('courses/answer_index.html')    
     context = RequestContext(request, {
         'answer_list': answer_list,
         'course_id': course_id,
@@ -77,3 +74,46 @@ def answer_index(request, question_id, course_id, lecture_id):
 
     return HttpResponse(template.render(context))
 
+def results(request, course_id, lecture_id, question_id):
+    q = get_object_or_404(Question, pk=question_id)
+    l = get_object_or_404(Lecture, pk=lecture_id)
+    return render(request, 'courses/results.html', {'question': q, 'lecture': l})
+
+def vote(request, course_id, lecture_id, question_id):
+    q = get_object_or_404(Question, pk=question_id)
+    l = get_object_or_404(Lecture, pk=lecture_id)
+    try:
+        selected_answer = q.answers.get(pk=request.POST['answer'])
+        print selected_answer
+    except (KeyError, Answer.DoesNotExist):
+        # Redisplay the poll voting form.
+        return render(request, 'courses/vote.html', {
+            'question':q,
+            'lecture':l,
+        })
+    else:
+        selected_answer.votes += 1
+        selected_answer.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('courses:results', args=(course_id, lecture_id, question_id,)))
+
+def answer(request, course_id, lecture_id, question_id):
+    q = get_object_or_404(Question, pk=question_id)
+    l = get_object_or_404(Lecture, pk=lecture_id)
+    try:
+        givenAnswer = request.POST['answer']
+        print givenAnswer
+    except (KeyError, Answer.DoesNotExist):
+        # Redisplay the poll answering form.
+        return render(request, 'courses/answer.html', {
+            'question': q,
+            'lecture': l,
+        })
+    else:
+        q.answers.create(answer_text=givenAnswer, votes=0)
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('courses:vote', args=(course_id, lecture_id, question_id,)))
