51d275b7af7ee68e05d90030d36085e31f8287c2
Floris Turkenburg <sk8_floris@hotmail.com>
2014-01-23 17:17:15 +0100

    Profile page set as homepage

--- Django/teacher/admin.py
+++ Django/teacher/admin.py
@@ -2,13 +2,17 @@ from django.contrib import admin
 from django.contrib.admin.sites import AdminSite
  
 from teacher.forms import UserAdminAuthenticationForm
+from teacher.views import profile_page
  
 class UserAdmin(AdminSite):
     # Anything we wish to add or override
     
     login_form = UserAdminAuthenticationForm
     login_template = 'admin/userLogin.html'
-    index_template = 'teacher/profilepage.html'
+    #index_template = 'teacher/profilepage.html'
+    
+    def index(self, request):
+        return profile_page(request)
     
     def has_permission(self, request):
         """
